f95c6345fb8623f6affa371b7943a278d5b2c14a
DMitte <mittedany547@gmail.com>
2023-05-02 22:42:09 -0500

    ultimos cambios

--- index.php
+++ index.php
@@ -31,5 +31,5 @@ include("foot.php")
 
     document.getElementById("blog-indice").innerHTML = "<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
-    document.getElementById("blog-relacionados").innerHTML = "<p><b>Temas relacionados:</b></p><hr> <p><a href='https://congresoutlvte.org/QuinonezBryan'>Quinonez Bryan</a></p><p><a href='eys-damaris.php'>Damaris Viallalva</a></p> ";
+    document.getElementById("blog-relacionados").innerHTML = "<p><b>Temas relacionados:</b></p><hr> <p><a href='https://congresoutlvte.org/AlvaradoNicolas'>Alvarado Nicolas</a></p><p><a href='https://congresoutlvte.org/ZambranoMarcelo'>Zambrano Marcelo</a></p> <p><a href='https://congresoutlvte.org/PaezFranklin'>Paez Franklin</a></p> <p><a href='https://congresoutlvte.org/CevillanoJenny'>Cevillano Jenny</a></p> ";
 </script>
